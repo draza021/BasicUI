b32697f463c62890db722f65ea7f02044c73e051
Dragoslav Ignjatovic <draza021@gmail.com>
2017-12-10 15:37:39 +0100

    added basic unit tests

---
+++ BasicUITests/BasicUITests.swift
@@ -0,0 +1,48 @@
+//
+//  BasicUITests.swift
+//  BasicUITests
+//
+//  Created by Drago on 12/10/17.
+//  Copyright © 2017 nsiddevelopment. All rights reserved.
+//
+
+import XCTest
+@testable import BasicUI
+
+class BasicUITests: XCTestCase {
+    
+    
+    //MARK: Meal Class Tests
+    
+    // Confirm that the meal initializer returns a Meal object when passed valid parameters
+    func testMealInitializationSucceeds() {
+        
+        // Zero rating
+        let zeroRatingMeal = Meal.init(name: "Zero", photo: nil, rating: 0)
+        XCTAssertNotNil(zeroRatingMeal)
+        
+        // Highest positive rating
+        let positiveRatingMeal = Meal.init(name: "Positive", photo: nil, rating: 5)
+        XCTAssertNotNil(positiveRatingMeal)
+        
+    }
+    
+    // Confirm that the Meal initialier returns nil when passed a negative rating or an empty name.
+    func testMealInitializationFails() {
+        
+        // Negative rating
+        let negativeRatingMeal = Meal.init(name: "Negative", photo: nil, rating: -1)
+        XCTAssertNil(negativeRatingMeal)
+        
+        // Rating exceeds maximum
+        let largeRatingMeal = Meal.init(name: "Large", photo: nil, rating: 6)
+        XCTAssertNil(largeRatingMeal)
+        
+        // Empty String
+        let emptyStringMeal = Meal.init(name: "", photo: nil, rating: 0)
+        XCTAssertNil(emptyStringMeal)
+    }
+    
+    
+    
+}
